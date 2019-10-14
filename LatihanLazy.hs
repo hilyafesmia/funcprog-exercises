27cd02b7f826727411f58dd2316e13e78e55d15a
hilyafesmia <hilyafesmia@gmail.com>
2019-10-14 15:34:50 +0700

    Added answers for questions 2 and 6 of Lazy Evaluation exercise

---
+++ LatihanLazy.hs
@@ -0,0 +1,13 @@
+-- 1. Uraikan langkah evaluasi dari ekspresi berikut: [​ x+y | x <- [1 .. 4], y <- [2 .. 4], x > y ]
+
+-- 2. Buatlah fungsi divisor yang menerima sebuah bilangan bulat ​n​ dan mengembalikan list bilangan bulat positif yang membagi habisn
+divisor n = [x | x <- [1..n], n `mod` x == 0]
+
+-- 3. Buatlah definisi ​quick sort​ menggunakan list comprehension.
+
+-- 4. Buatlah definisi infinite list untuk permutation.
+
+-- 5. Buatlah definisi untuk memberikan infinite list dari bilangan prima menerapkan a l g o r i t m a S​ i e v e o f E r a s t o t h e n e s .​
+
+-- 6. Buatlah definisi infinite list dari ​triple pythagoras.​ List tersebut terdiri dari element triple bilangan bulat positif yang mengikut persamaan pythagoras x​ 2​ ​ + y2​ ​ = z2​ ​ ​.
+pythaTriple = [(x, y, z) | z <- [5..], y <- [4..z], x <- [3..y], x^2 + y^2 == z^2 ]
