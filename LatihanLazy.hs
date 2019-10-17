76118a9c9e972df5d4a603e732010efbcd27ac19
hilyafesmia <hilyafesmia@gmail.com>
2019-10-18 02:37:47 +0700

    1st try on sieveOfErastothenes, still failing

--- LatihanLazy.hs
+++ LatihanLazy.hs
@@ -9,6 +9,7 @@ quickSort (x:xs) = quickSort [ y | y <- xs, y <= x ] ++ [x] ++ quickSort [ y | y
 -- 4. Buatlah definisi infinite list untuk permutation.
 
 -- 5. Buatlah definisi untuk memberikan infinite list dari bilangan prima menerapkan a l g o r i t m a S​ i e v e o f E r a s t o t h e n e s .​
+sieveOfErastothenes (x:xs) = [ y | y <- xs, y `mod` x /= 0, y /= x]
 
 -- 6. Buatlah definisi infinite list dari ​triple pythagoras.​ List tersebut terdiri dari element triple bilangan bulat positif yang mengikut persamaan pythagoras x​ 2​ ​ + y2​ ​ = z2​ ​ ​.
 pythaTriple = [(x, y, z) | z <- [5..], y <- [4..z], x <- [3..y], x^2 + y^2 == z^2 ]
