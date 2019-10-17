90a63d831e7203398e965dec88af70d8a5104292
hilyafesmia <hilyafesmia@gmail.com>
2019-10-18 03:28:30 +0700

    Tried solving unanswered questions from quiz 1

---
+++ ReviewQuiz1.hs
@@ -0,0 +1,18 @@
+-- Beberapa soal quiz 1 yang tidak saya kerjakan dan/atau yang nilainya belum full score
+
+-- 1. Tentukan tipe dan buat definisi fungsi maxTiga yang menerima 3 buah bilangan positif dam menghasilkan bilangan terbesar dari bilangan input tersebut
+maxTiga :: Int -> Int -> Int -> Int
+maxTiga a b c = [ a | a > b, a > c ] || [ b | b > a, b > c ] || [ c | c > a, c > b ]
+
+-- 6. Definisikan fungsi primes yang merupakan sebuah infinite list dari bilangan prima menggunakan algoritma Sieve of Erastothenes
+-- Ada pada file LatihanLazy.hs
+
+-- 7. Tentukan tipe dan buat definisi fungsi flip
+-- Ada pada file LatihanHOF1.hs
+
+-- Soal 8, 9, 10 menggunakan kodingan monad robot
+-- 8. Tentukan tipe fungsi updateState
+
+-- 9. Definisikan fungsi turnRight
+
+-- 10. Tentukan tipe fungsi toEnum pada fungsi right
