523f0ecbd039ebc830752f6aa8b5a49380641170
hilyafesmia <hilyafesmia@gmail.com>
2019-10-19 15:21:43 +0700

    Added subst and evaluate expr

--- BahasaAritmatikaSederhana.hs
+++ BahasaAritmatikaSederhana.hs
@@ -14,14 +14,34 @@ foldrTree f start (Branch t1 t2) = f (foldrTree f start t1) (foldrTree f start t
 
 
 -- 3. Tambahkan konstruksi Let pada struktur data Ekspresi menjadi:
--- ​data​​ Expr​​ = ​C Float | Expr :+ Expr | Expr :- Expr 
---          | Expr :* Expr | Expr :/ Expr
---          | Let String Expr Expr ​
---      deriving​ Show
+data Expr = C Float | Expr :+ Expr | Expr :- Expr
+          | Expr :* Expr | Expr :/ Expr 
+          | V [Char]
+          | Let String Expr Expr      
+    deriving Show
 
 
 -- 4. Sesuaikan fungsi evaluasi versi sebelumnya (versi definisi rekursif).
 
+subst :: String -expr -expr -expr
+
+subst v0 e0 (V v1)         = if (v0 == v1) then e0 else (V v1)
+subst v0 e0 (C c)          = (C c)
+subst v0 e0 (e1 :+ e2)     = subst v0 e0 e1 :+ subst v0 e0 e2
+subst v0 e0 (e1 :- e2)     = subst v0 e0 e1 :+ subst v0 e0 e2
+subst v0 e0 (e1 :* e2)     = subst v0 e0 e1 :+ subst v0 e0 e2
+subst v0 e0 (e1 :/ e2)     = subst v0 e0 e1 :+ subst v0 e0 e2
+subst v0 e0 (Let v1 e1 e2) = Let v1 e1 (subst v0 e0 e2)
+
+evaluate :: Expr -> Float
+
+evaluate (C x) = x
+evaluate (e1 :+ e2)    = evaluate e1 + evaluate e2
+evaluate (e1 :- e2)    = evaluate e1 - evaluate e2
+evaluate (e1 :* e2)    = evaluate e1 * evaluate e2
+evaluate (e1 :/ e2)    = evaluate e1 / evaluate e2
+evaluate (Let v e0 e1) = evaluate (subst v e0 e1)
+evaluate (V v)         = 0.0   
 
 -- 5. Sesuaikan fungsi evaluasi versi menggunakan fold.
 
