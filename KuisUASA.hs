3dc697471056417d7392ce2221f2926f53f3178d
Hilya Auli Fesmia <hilyafesmia@gmail.com>
2019-12-13 10:53:35 +0700

    Added answer to question number 2

--- KuisUASA.hs
+++ KuisUASA.hs
@@ -1,7 +1,12 @@
 -- 1. Given a list of words, remove from the list all those which contain four or more vowels and those which have same letter appearing twice (or more) in a row. In addition, any word containing numbers should have the numbers removed. Note that the number removal should happen before any other operations so that the subsequent operations can remove the word if necessary.
 
 -- 2. Writeafunctionr​ otabc​thatchanges​a'​stob​ '​s,b​ ​'stoc​ ​'sandc​ '​stoa​ ​'sinastring. Only lowercase letters are affected.
-
+rotabc = map abc 
+  where abc 'a' = 'b'
+        abc 'b' = 'c'
+        abc 'c' = 'a'
+        abc  x  =  x
+        
 -- 3. Definisikan fungsi ​last​, dengan menerapkan p​ oint-free style.​ Fungsi l​ ast​ tersebut menerima sebuah list dan mengembalikan elemen terakhir dari list tersebut.
 
 -- 4. Sebagaimanamaterikuliahterkait​ComposingContract,​diperlihatkansebuah contract​ yang disebut Z​ ero-Coupon Bound (z​ cb​).​ Pemanggilan fungsi ​zcb t x k,​ menyatakan bahwa pada waktu ​t,​ ​contract​ ini akan senilai dengan ​x​ pada kurs ​k​ . Misalkan fungsi contract definisi lain sudah tersedia. Bagaimana mengkomposisikan nya untuk mendefinisikan fungsi z​ cb​. Fungsi yang boleh anda gunakan adalah antara lain: (w​ hen, give, and, or, at, scale, konst, one, zero)​
